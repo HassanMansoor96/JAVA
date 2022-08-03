bb9dc4461486b05dc95455cb30a1d04e8423c77e
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-03 12:39:50 +0200

    Book3 Chapter6

---
+++ Java All-in-one For Dummies/Book3/src/Chapter6/TestToString.java
@@ -0,0 +1,30 @@
+//The toString Method
+
+//The toString method returns a String representation of an object. By default, the
+//toString method returns the name of the object’s class plus its hash code.
+        
+        
+package Chapter6;
+
+public class TestToString {
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("Martinez",
+                "Anthony");
+        System.out.println(emp.toString());
+    }
+}
+
+class Employee {
+
+    private String lastName;
+    private String firstName;
+
+    public Employee(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+}
+
+//This code creates a new Employee object; then the result of its toString method
+//is printed to the console.
