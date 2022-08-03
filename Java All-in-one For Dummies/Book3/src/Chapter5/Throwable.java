488360f27d404d547789fa83c0d51ad614a163ce
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-03 12:05:15 +0200

    Book3 Chapter5

---
+++ Java All-in-one For Dummies/Book3/src/Chapter5/Throwable.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Chapter5;
+
+
+//Extending interfaces
+
+//You can extend interfaces by using the extends keyword. An interface that extends
+//an existing interface is called a subinterface, and the interface being extended is
+//called the superinterface.
+
+public interface ThrowableBall {
+
+    void throwBall();
+
+    void catchBall();
+}
+
+public interface KickableBall {
+
+    void kickBall();
+
+    void catchBall();
+}
+
+public interface PlayableBall
+        extends ThrowableBall, KickableBall {
+
+    void dropBall();
+}
+
+//Here three interfaces are declared. The first, named ThrowableBall, defines two
+//methods: throwBall and catchBall. The second, named KickableBall, also
+//defines two methods: kickBall and catchBall. The third, named PlayableBall,
+//extends ThrowableBall and KickableBall, and adds a method of its own named
+//dropBall.
