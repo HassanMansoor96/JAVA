be80594e573a9163eff106b936c7845dc5283a08
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-10 12:15:21 +0200

    Book3 chapter 7

---
+++ Java All-in-one For Dummies/Book3/src/Chapter7/OuterClassName.java
@@ -0,0 +1,15 @@
+
+//Declaring Inner Classes
+
+
+package Chapter7;
+
+//An inner class is a class that’s declared inside another class. Thus the basic structure
+//for creating an inner class is as follows:
+
+class outerClassName {
+
+    private class innerClassName {
+// body of inner class
+    }
+}
