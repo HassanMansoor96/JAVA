284a8d86f047bd070560e3d9d542e0f36201f92b
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-10 12:20:21 +0200

    Book3 chapter 7

---
+++ Java All-in-one For Dummies/Book3/src/Chapter7/OuterClassName2.java
@@ -0,0 +1,10 @@
+//Using Static Inner Classes
+package Chapter7;
+
+//A static inner class is similar to an inner class
+class outerClassName2 {
+
+    private static class innerClassName {
+        // body of inner class
+    }
+}
