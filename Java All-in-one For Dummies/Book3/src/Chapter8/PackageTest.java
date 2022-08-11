4809b1f09776df370f712c2510f2e24b3f234221
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-11 09:40:39 +0200

    Book3 chapter 8

---
+++ Java All-in-one For Dummies/Book3/src/Chapter8/PackageTest.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter8;
+
+/**
+ *
+ * @author Hassan
+ */
+public class PackageTest {
+
+    public static void main(String[] args) {
+        while (Console.askYorN("Keep going?")) {
+            System.out.println("D'oh!");
+        }
+    }
+}
