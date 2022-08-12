a6cee463d24b8c5226eeb1a669ec2d005900e88b
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-12 09:47:42 +0200

    book3 chapter8

--- Java All-in-one For Dummies/Book3/src/Chapter8/Console.java
+++ Java All-in-one For Dummies/Book3/src/Chapter8/Console.java
@@ -1,7 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+//Working with Packages
+
+//A package is a group of classes that belong together.
+
+
+//Importing classes and packages
+
+//When you use import statements at the beginning of a Java source file, you make
+//classes from the packages mentioned in the import statements available throughout
+//the file.
+
+//An import statement can import all the classes in a package by using an asterisk
+//wildcard. Here all the classes in the java.util package are imported:
+
+//import java.util.*;
+
+//Alternatively, you can import classes one at a time. Here just the ArrayList class is imported:
+
+//import java.util.ArrayList;
+
+//Note: You don’t have to use an import statement to use a class from a package.
+//But if you don’t use an import statement, you must fully qualify any references to
+//the class. For example, you can use the ArrayList class without importing java.
+
+//util:
+
+//java.util.ArrayList = new java.util.ArrayList();
+
+//Because fully qualified names are a pain to always spell out, you should always
+//use import statements to import the packages or individual classes your application
+//uses.
+
 package Chapter8;
 
 /**
