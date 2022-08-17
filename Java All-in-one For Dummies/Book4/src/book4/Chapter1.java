ce5273c719267f0b6625c423a492fe333c8fa2ba
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-17 12:12:17 +0200

    Book4 Chapter1

--- Java All-in-one For Dummies/Book4/src/book4/Chapter1.java
+++ Java All-in-one For Dummies/Book4/src/book4/Chapter1.java
@@ -1,7 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
+//Working with Strings
+
+//Strings are among the most common types of objects in Java.
+
+//Strings are reference types, not value types, such as int or boolean. As a
+//result, a string variable holds a reference to an object created from the
+//String class, not the value of the string itself.
+
+//Even though strings aren’t primitive types, the Java compiler has some
+//features designed to let you work with strings almost as though they were
+//primitive types. Java lets you assign string literals to string variables, for
+//example, like this:
+//String line1 = "Oh what a beautiful morning!";
+
+//Strings can include escape sequences that consist of a slash followed by
+//another character. The most common escape sequences are \n for new line
+//and \t for tab. If you want to include a back slash in a string, you must use the
+//escape sequence \\. Here is a complete list of all the escape sequences you
+//can use:
+
+//Escape Sequence Explanation
+//\n Newline
+//\t Tab
+//\b Backspace
+//\r Carriage return
+//\f Form feed
+//\' Apostrophe
+//\" Quotation mark
+//\\ Back slash
+
+//Strings and characters are different. String literals are marked by quotation
+//marks; character literals are marked by apostrophes. Thus, "a" is a string
+//literal that happens to be one character long. By contrast, 'a' is a character
+//literal.
+
 package book4;
 
 /**
