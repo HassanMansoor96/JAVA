6d165db5895c3d983114480d27fa4bb436f3fb33
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-17 12:14:50 +0200

    Book4 Chapter1

--- Java All-in-one For Dummies/Book4/src/book4/Chapter1.java
+++ Java All-in-one For Dummies/Book4/src/book4/Chapter1.java
@@ -34,6 +34,39 @@
 //literal that happens to be one character long. By contrast, 'a' is a character
 //literal.
 
+//You can combine, or concatenate, strings by using the + operator, like this:
+//String line2 = line1 + “\nOh what a beautiful day!”;
+
+//You can also use the += operator with strings, like this:
+//line2 += “\nI’ve got a beautiful feeling”;
+
+//When a primitive type is used in a concatenation expression, Java automatically
+//converts the primitive type to a string. Thus Java allows the following:
+//int empCount = 50;
+//String msg = "Number of employees: " + empCount;
+
+//The various primitive wrapper classes (such as integer and double) have
+//parse methods that can convert string values to numeric types. Here’s an
+//example:
+//String s = "50";
+//int i = Integer.parseInt(s);
+
+//You can’t compare strings by using the equality operator (==). Instead, you
+//should use the equals method. Here’s an example:
+//if (lastName.equals("Lowe"))
+//System.out.println("This is me!");
+
+//The String class also has an equalsIgnoreCase method that compares
+//strings without considering case. Here’s an example:
+//if (lastName.equalsIgnoreCase("lowe"))
+//System.out.println("This is me again!");
+
+
+
+
+
+
+
 package book4;
 
 /**
