38dc3ca0841509524a9f52d1fa978abf45c510bd
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-08-17 12:23:03 +0200

    Book4 Chapter1

---
+++ Java All-in-one For Dummies/Book4/src/book4/UsingStringClass.java
@@ -0,0 +1,63 @@
+
+//Using the String Class
+
+//The String class is the class you use to create string objects. It has a whole gaggle
+//of methods that are designed to let you find out information about the string
+//that’s represented by the String class.
+
+//String Class Methods
+
+//Method                                Description
+
+//char charAt(int)                      Returns the character at the specified position in the string.
+
+//int compareTo(String)                 Compares this string to another string, using alphabetical
+//                                      order. Returns -1 if this string comes before the other string, 0
+//                                      if the strings are the same, and 1 if this string comes after the
+//                                      other string.
+
+//int compareToIgnoreCase(String)       Similar to compareTo but ignores case.
+
+//boolean contains(CharSequence)        Returns true if this string contains the parameter value.
+//                                      The parameter can be a String, StringBuilder, or
+//                                      StringBuffer.
+
+//boolean endsWith(String)              Returns true if this string ends with the parameter string.
+
+//boolean equals(String)                Returns true if this string has the same value as the
+//                                      parameter string.
+
+//boolean equalsIgnoreCase(String)      Similar to equals but ignores case.
+
+//int indexOf(char)                     Returns the index of the first occurrence of the char parameter
+//                                      in this string. Returns -1 if the character isn’t in the string.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package book4;
+
+/**
+ *
+ * @author Hassan
+ */
+public class UsingStringClass {
+    
+}
