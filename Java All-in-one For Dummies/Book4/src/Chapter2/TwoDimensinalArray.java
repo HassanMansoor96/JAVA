f96b0dfa16d19c8bce70c3430368f69afa5857c9
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-01 08:57:04 +0200

    Book4 Chapter2

---
+++ Java All-in-one For Dummies/Book4/src/Chapter2/TwoDimensinalArray.java
@@ -0,0 +1,51 @@
+//Using Two-Dimensional Arrays
+
+//The elements of an array can be any type of object you want, including another
+//array. In the latter case, you have a two-dimensional array, sometimes called an
+//array of arrays.
+
+//Two-dimensional arrays are often used to track data in column-and-row format,
+//much the way that a spreadsheet works
+
+
+//Creating a two-dimensional array
+
+//To declare a two-dimensional array for this sales data, you simply list two sets of
+//empty brackets, like this:
+
+//double sales[][];
+
+//Here sales is a two-dimensional array of type double. To put it another way,
+//sales is an array of double arrays.
+
+//To create the array, you use the new keyword and provide lengths for each set of
+//brackets, as in this example:
+
+//sales = new double[5][4];
+
+//Here the first dimension specifies that the sales array has five elements. This
+//array represents the rows in the table. The second dimension specifies that each
+//of those elements has an array of type double with four elements. This array represents
+//the columns in the table.
+
+//Note that as with a one-dimensional array, you can declare and create a twodimensional
+//array in one statement, like this:
+
+//double[][] sales = new double[5][4];
+
+//Here the sales array is declared and created all in one statement.
+
+
+
+
+
+
+package Chapter2;
+
+/**
+ *
+ * @author Hassan
+ */
+public class TwoDimensinalArray {
+    
+}
