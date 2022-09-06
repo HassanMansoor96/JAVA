bb4eca1ff04eeaf88f06c3df3de17dc9b15fe5a2
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-06 17:27:59 +0200

    Book5 Chapter4

---
+++ Java All-in-one For Dummies/Book5/Chapter4DatesTimes/src/page543_UsingNow/TimeTester.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package page543_UsingNow;
+
+
+import java.util.*;
+import java.time.*;
+
+public class TimeTester {
+
+    public static void main(String[] args) {
+        System.out.println("\nLocalTime: "
+                + LocalTime.now().toString());
+        System.out.println("\nLocalDateTime: "
+                + LocalDateTime.now().toString());
+        System.out.println("\nZonedDateTime: "
+                + ZonedDateTime.now().toString());
+        System.out.println("\nOffsetTime: "
+                + OffsetTime.now().toString());
+        System.out.println("\nOffsetDateTime: "
+                + OffsetDateTime.now().toString());
+        System.out.println("\nMonthDay: "
+                + MonthDay.now().toString());
+        System.out.println("\nYearMonth: "
+                + YearMonth.now().toString());
+        System.out.println("\nInstant: "
+                + Instant.now().toString());
+    }
+}
