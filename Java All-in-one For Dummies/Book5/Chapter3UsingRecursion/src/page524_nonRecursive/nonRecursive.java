10cf010e7b3ed4c5bf8bbd027697f9a249521641
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-06 17:27:08 +0200

    Book5 Chapter3

---
+++ Java All-in-one For Dummies/Book5/Chapter3UsingRecursion/src/page524_nonRecursive/nonRecursive.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package page524_nonRecursive;
+
+
+public class nonRecursive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n = 5;
+        long fact;
+        fact = factorial(n);
+        System.out.println("The factorial of " + n + " is "
+                + fact + ".");
+    }
+
+    private static long factorial(int n) {
+        long f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        return f;
+    }
+}
---
+++ Java All-in-one For Dummies/Book5/Chapter3UsingRecursion/src/page525_recursive/recursive.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package page525_recursive;
+
+
+public class recursive {
+
+    public static void main(String[] args) {
+        int n = 5;
+        long fact;
+        fact = factorial(n);
+        System.out.println("The factorial of " + n + " is "
+                + fact + ".");
+    }
+
+    private static long factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
