26110134ecc57590cf35589095d8f62c8aa50007
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-21 09:08:26 +0200

    Book8 Chapter5

---
+++ Java All-in-one For Dummies/Book8/book08/src/main/java/com/example/book08/chapter5/page863Attributes.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.book08.chapter5;
+
+/**
+ *
+ * @author Hassan
+ */
+public class page863Attributes {
+    
+}
+
+/*
+Attributes
+Instead of using child elements, you can use attributes to provide data for an
+element.
+An attribute is a name-and-value pair that’s written inside the start tag
+for an element. Here’s a Movie element that uses an attribute instead of a child
+element to record the year:
+
+<Movie year="1946">
+<Title>It's a Wonderful Life</Title>
+<Price>14.95</Price>
+</Movie>
+
+Whether you use attributes or child elements is largely a matter of personal preference.
+Many XML purists say that you should avoid using attributes or use them only to 
+identify data such as identification numbers or codes; others suggest using
+attributes freely. In my experience, a few attributes here and there don’t hurt, but
+for the most part I avoid using them.
+*/
