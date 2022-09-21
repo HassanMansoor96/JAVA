4c09b3287d240a32fb36db2117dc5c3cef8a3034
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-21 09:09:31 +0200

    Book8 Chapter5

---
+++ Java All-in-one For Dummies/Book8/book08/src/main/java/com/example/book08/chapter5/page864TheXMLdeclararion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.book08.chapter5;
+
+/**
+ *
+ * @author Hassan
+ */
+public class page864TheXMLdeclararion {
+    
+}
+
+/*
+The XML declaration
+Every XML file should begin with an XML declaration that identifies the version of
+XML being used. For most XML documents, the declaration should look like this:
+<?xml version='1.0'?>
+Note that the XML declaration does not require an end tag. The XML declaration
+should be the first line in the file.
+*/
