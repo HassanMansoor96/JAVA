0ce145bbca4dd08495f26c9de8baecb82cf82e62
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-22 12:31:31 +0200

    Book8 Chapter5

---
+++ Java All-in-one For Dummies/Book8/book08/src/main/java/com/example/book08/chapter5/page867AttributeTypes.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.book08.chapter5;
+
+/**
+ *
+ * @author Hassan
+ */
+public class page867AttributeTypes {
+    
+}
+
+/*
+Attribute Types
+
+Element                 The Attribute Value . . .
+CDATA                   Can be any character string.
+(string1|string2...)    Can be one of the listed strings.
+NMTOKEN                 Must be a name token, which is a string made up of letters and numbers.
+NMTOKENS                Must be one or more name tokens separated by white space.
+ID                      Is a name token that must be unique. In other words, no other element
+                        in the document can have the same value for this attribute.
+IDREF                   Must be the same as an ID value used elsewhere in the document.
+IDREFS                  Is a list of IDREF values separated by white space.
+*/
