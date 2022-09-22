7702f4823e7c08c515739e617897da2207a6d116
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-22 12:29:12 +0200

    Book8 Chapter5

---
+++ Java All-in-one For Dummies/Book8/book08/src/main/java/com/example/book08/chapter5/page867SpecifyingElementContent.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.book08.chapter5;
+
+/**
+ *
+ * @author Hassan
+ */
+public class page867SpecifyingElementContent {
+    
+}
+
+/*
+Specifying Element Content
+
+Content             Description
+element*            The specified element can occur 0 or more times.
+element+            The specified element can occur 1 or more times.
+element?            The specified element can occur 0 or 1 time.
+element1|element2   Either element1 or element2 can appear.
+element1, element2  element1 appears, followed by element2.
+#PCDATA             Text data is allowed.
+ANY                 Any child elements are allowed.
+EMPTY               No child elements of any type are allowed.
+*/
