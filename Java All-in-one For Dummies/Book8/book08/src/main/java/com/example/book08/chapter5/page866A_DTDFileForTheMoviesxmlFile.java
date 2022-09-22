0f305ea4e453c5a4aa596f825cb343872bef45f2
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-22 12:24:58 +0200

    Book8 Chapter5

---
+++ Java All-in-one For Dummies/Book8/book08/src/main/java/com/example/book08/chapter5/page866A_DTDFileForTheMoviesxmlFile.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.book08.chapter5;
+
+/**
+ *
+ * @author Hassan
+ */
+public class page866A_DTDFileForTheMoviesxmlFile {
+    
+}
+
+/*
+<?xml version="1.0" encoding="UTF-8"?>
+<!ELEMENT Movies (Movie*)>
+<!ELEMENT Movie (Title, Price)>
+<!ATTLIST Movie year CDATA #REQUIRED>
+<!ELEMENT Title (#PCDATA)>
+<!ELEMENT Price (#PCDATA)>
+*/
