21177513007bba8fd0d0f0b53a40471e753b9df1
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-23 10:45:48 +0200

    Book8 Chapter5

--- Java All-in-one For Dummies/Book8/book08/src/main/java/com/example/book08/chapter5/page868AttributeDefaults.java
+++ Java All-in-one For Dummies/Book8/book08/src/main/java/com/example/book08/chapter5/page868AttributeDefaults.java
@@ -21,4 +21,14 @@ value Optional. This value is used if the attribute is omitted.
 and if omitted, this value is used by default.
 */
 
-
+/*
+Here’s the ATTLIST tag declaration from movies.dtd:
+<!ATTLIST Movie year CDATA #REQUIRED>
+This declaration indicates that the attribute goes with the Movie element, is
+named year, can be any kind of data, and is required.
+Here’s an ATTLIST tag that specifies a list of possible values along with a default:
+<!ATTLIST Movie genre (SciFi|Action|Comedy|Drama) Comedy>
+This form of the ATTLIST tag lets you create an attribute that’s similar to an
+enumeration,
+with a list of acceptable values.
+*/
