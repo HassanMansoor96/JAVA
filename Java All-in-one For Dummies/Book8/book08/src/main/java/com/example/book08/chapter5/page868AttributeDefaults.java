b9af38f74567b6cc1d7dc3462f4b34c985aa8445
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-23 10:45:27 +0200

    Book8 Chapter5

---
+++ Java All-in-one For Dummies/Book8/book08/src/main/java/com/example/book08/chapter5/page868AttributeDefaults.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.book08.chapter5;
+
+/**
+ *
+ * @author Hassan
+ */
+public class page868AttributeDefaults {
+    
+}
+/*
+Attribute Defaults
+Default Optional or Required?
+#REQUIRED Required.
+#IMPLIED Optional.
+value Optional. This value is used if the attribute is omitted.
+#FIXED value Optional. If included, however, it must be this value,
+and if omitted, this value is used by default.
+*/
+
+
