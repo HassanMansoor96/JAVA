d7794b0870aaa1612f809ec52a68dfbd88651aa6
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-09-27 11:50:07 +0200

    Book5 Chapter3

--- Java All-in-one For Dummies/Book5/Chapter3UsingRecursion/src/page524_nonRecursive/nonRecursive.java
+++ Java All-in-one For Dummies/Book5/Chapter3UsingRecursion/src/page524_nonRecursive/nonRecursive.java
@@ -1,4 +1,13 @@
 /*
+Using Recursion
+
+Recursion is a basic programming technique in which a method calls itself to
+solve some problem. A method that uses this technique is called recursive.
+Many programming problems can be solved only by recursion, and some
+problems that can be solved by other techniques are better solved by recursion.
+*/
+
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -17,7 +26,11 @@ public class nonRecursive {
         System.out.println("The factorial of " + n + " is "
                 + fact + ".");
     }
-
+    
+    /*
+    This method uses a for loop to count from 1 to the number, keeping track of the
+    product as it goes.
+    */
     private static long factorial(int n) {
         long f = 1;
         for (int i = 1; i <= n; i++) {
